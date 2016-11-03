4d3b2e5b63f3147981a7430b3d0eeb39d8def6ca
ubulem <berkut87@gmail.com>
2016-11-03 17:13:02 +0300

    Initial commit

---
+++ android/src/com/berkut/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.berkut;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.berkut.Love;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new Love(), config);
+    }
+}
